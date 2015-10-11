0a9596c25785dec5d567ff705c562b70c4ff2025
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-10-11 18:59:54 +0530

    feat(ExTop): add scrolling

--- lib/ex_top.ex
+++ lib/ex_top.ex
@@ -1,7 +1,7 @@
 defmodule ExTop do
   use GenServer
 
-  defstruct [:data, selected: 0]
+  defstruct [:data, selected: 0, offset: 0]
 
   def start_link(opts \\ []) do
     GenServer.start_link ExTop, opts
@@ -27,27 +27,27 @@ defmodule ExTop do
 
   # Up Arrow
   def handle_info({port, {:data, "\e[A" <> rest}}, state) do
-    selected = if state.selected == 0 do
-      0
-    else
-      state.selected - 1
-    end
+    state = case {state.selected, state.offset} do
+              {0, 0} -> state
+              {0, n} -> %{state | offset: n - 1}
+              {n, _} -> %{state | selected: n - 1}
+            end
     send self, {port, {:data, rest}}
     GenServer.cast self, :render
-    {:noreply, %{state | selected: selected}}
+    {:noreply, state}
   end
 
   # Down Arrow
   def handle_info({port, {:data, "\e[B" <> rest}}, state) do
     max = Enum.count(state.data[:processes]) - 1
-    selected = if state.selected >= max do
-      max
-    else
-      state.selected + 1
+    state = cond do
+      state.offset + state.selected + 1 >= max -> state
+      state.selected == 19 -> %{state | offset: state.offset + 1}
+      true -> %{state | selected: state.selected + 1}
     end
     send self, {port, {:data, rest}}
     GenServer.cast self, :render
-    {:noreply, %{state | selected: selected}}
+    {:noreply, state}
   end
 
   # Empty
@@ -57,7 +57,9 @@ defmodule ExTop do
 
   def handle_cast(:render, state) do
     # Only show 20 processes at once.
-    processes = Enum.take(state.data[:processes], 20)
+    processes = state.data[:processes]
+                |> Enum.drop(state.offset)
+                |> Enum.take(20)
     data = %{state.data | processes: processes}
     IO.write [IO.ANSI.home,
               ExTop.View.render(data, selected: state.selected)]
