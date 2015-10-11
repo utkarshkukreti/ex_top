7057736786ec2dd72c932cbff4b3e08e5a2e31d1
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-10-11 18:02:13 +0530

    feat(ExTop.Collector)

---
+++ lib/ex_top/collector.ex
@@ -0,0 +1,13 @@
+defmodule ExTop.Collector do
+  def collect do
+    processes = for pid <- :erlang.processes do
+      info = :erlang.process_info(pid, [:memory,
+                                        :message_queue_len,
+                                        :reductions,
+                                        :registered_name])
+      [{:pid, pid} | info]
+    end
+
+    [processes: processes]
+  end
+end
