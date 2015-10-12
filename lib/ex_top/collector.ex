3aa1cd35c2051f89ae6777e877e4a28edeeecb36
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-10-12 16:46:44 +0530

    feat(ExTop.Collector): collect some system stats

--- lib/ex_top/collector.ex
+++ lib/ex_top/collector.ex
@@ -9,8 +9,21 @@ defmodule ExTop.Collector do
                                         :registered_name])
       [{:pid, pid} | info]
     end
+    {{:input, io_input}, {:output, io_output}} = :erlang.statistics(:io)
+    run_queue = :erlang.statistics(:run_queue)
+    process_count = :erlang.system_info(:process_count)
+    process_limit = :erlang.system_info(:process_limit)
+    uptime = :erlang.statistics(:wall_clock) |> elem(0) |> div(1000)
 
     %{memory: memory,
-      processes: processes}
+      processes: processes,
+      system: %{
+        io_input: io_input,
+        io_output: io_output,
+        process_count: process_count,
+        process_limit: process_limit,
+        run_queue: run_queue,
+        uptime: uptime
+      }}
   end
 end
